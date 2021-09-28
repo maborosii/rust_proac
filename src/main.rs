394f0b75cea4c70a26dbca038b27a51bed498c70
maborosi <bonvovage@163.com>
2021-09-28 16:06:24 +0800

    init

---
+++ src/main.rs
@@ -0,0 +1,26 @@
+fn main() {
+    // let tup = (3, 6.1, 'a');
+    // let (x, y, z) = tup;
+    // println!("{}\t{}\t{}", x, z, y);
+    //
+    // let a = tup.0;
+    // println!("{}", a);
+    //
+    // let x = 5;
+    // let y = {
+    // let x = 3;
+    // println!("x of inner is: {}", x);
+    // x + 1
+    // };
+    // println!("x: {}\ty: {}", x, y);
+    //
+    // let a = [1, 2, 3, 4, 5];
+    // for element in a.iter() {
+    //     println!("element is {}", element)
+    // }
+    // for element in (1..5).rev() {
+    //     println!("reverse element is {}", element);
+    // }
+    let s = String::from("hello");
+    println!("{}", s);
+}
